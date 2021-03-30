39096caf93c387f19a86232b164bad2702fee338
Roman Valovič <xvalovic@node.mendelu.cz>
2021-03-30 08:50:53 +0200

    code of seminar 7

--- src/routes.php
+++ src/routes.php
@@ -27,7 +27,19 @@ $app->get('/persons', function (Request $request, Response $response, $args) {
 	$tplVars['persons_list'] = $stmt->fetchall(); # [ ['id_person' => 1, 'firs_name'=> 'johny'...], ['id_person' => 2... ]  ]
 	#echo var_dump($persons);
 	return $this->view->render($response, 'persons.latte', $tplVars);
-});
+})->setName('persons');
 
 
-
+/* Vyhladavnie osob */
+$app->get('/person/search', function(Request $request, Response $response, $args) {
+    // localhost:2000/public/persons?query=carl&height=25 [query => 'carl', height => 25]
+    $queryParams = $request->getQueryParams();
+    if (! empty($queryParams) && ! empty($queryParams['query'])) {
+       $stmt = $this->db->prepare('SELECT first_name, last_name, nickname, height FROM person WHERE lower(first_name) = lower(:fname) OR lower(last_name) = lower(:lname) ORDER BY first_name');
+       $stmt->bindParam(':fname', $queryParams['query']);
+       $stmt->bindParam(':lname', $queryParams['query']);
+       $stmt->execute();
+       $tplVars['persons_list'] = $stmt->fetchall(); 
+       return $this->view->render($response, 'persons.latte', $tplVars);
+    }
+})->setName('search');
